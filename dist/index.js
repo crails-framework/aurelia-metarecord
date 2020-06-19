c9d6bcdc88eef93f385cbf3c920e31aebb5ca11d
Michael <michael@unetresgrossebite.com>
2020-06-19 22:13:28 +0200

    adds collection exports to index.js

--- dist/index.js
+++ dist/index.js
@@ -3,7 +3,7 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.Model = exports.Fields = undefined;
+exports.PagedCollection = exports.Collection = exports.Model = exports.Fields = undefined;
 
 var _fields = require("./fields");
 
@@ -13,7 +13,13 @@ var _model = require("./model");
 
 var ModelModule = _interopRequireWildcard(_model);
 
+var _collection = require("./collection");
+
+var CollectionModule = _interopRequireWildcard(_collection);
+
 function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) newObj[key] = obj[key]; } } newObj.default = obj; return newObj; } }
 
 var Fields = exports.Fields = FieldsModule.Fields;
 var Model = exports.Model = ModelModule.Model;
+var Collection = exports.Collection = CollectionModule.Collection;
+var PagedCollection = exports.PagedCollection = CollectionModule.PagedCollection;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import * as FieldsModule from "./fields";
 import * as ModelModule from "./model";
+import * as CollectionModule from "./collection";
 
 export const Fields = FieldsModule.Fields;
 export const Model = ModelModule.Model;
+export const Collection = CollectionModule.Collection;
+export const PagedCollection = CollectionModule.PagedCollection;
